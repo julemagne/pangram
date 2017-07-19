8010a83bd5096ceae619b07a3a6b7be8ef5e9c92
Julie David <julie.angela.david@gmail.com>
2017-07-18 20:12:04 -0400

    Use regex method

--- pangram.rb
+++ pangram.rb
@@ -1,26 +1,30 @@
 class Pangram
 
-  def self.pangram? phrase
-    return false if phrase == ''
+  # def self.pangram? phrase
+  #   return false if phrase == ''
+  #
+  #   phrase = phrase.downcase
+  #
+  #   alphabet = Array('a'..'z')
+  #
+  #   a = alphabet.each_with_object Hash.new do |(k, v), h|
+  #     h[k] = false
+  #   end
+  #
+  #   phrase.chars.each do |letter|
+  #     a[letter] = true
+  #   end
+  #
+  #   if a.values.include? false
+  #     return false
+  #   else
+  #     return true
+  #   end
+  #
+  # end
 
-    phrase = phrase.downcase
-    
-    alphabet = Array('a'..'z')
-
-    a = alphabet.each_with_object Hash.new do |(k, v), h|
-      h[k] = false
-    end
-
-    phrase.chars.each do |letter|
-      a[letter] = true
-    end
-
-    if a.values.include? false
-      return false
-    else
-      return true
-    end
-
+  def self.pangram? str
+    letters = str.gsub(/[^a-z]/i,"")
+    letters.downcase.chars.uniq.count == 26
   end
-
 end
